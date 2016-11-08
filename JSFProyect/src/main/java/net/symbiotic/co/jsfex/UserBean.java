ef0a85513f11ddeed1f5eeed4fa1f29c33c2aea5
Ricardo Apú <rapuc@symbiotic.net>
2016-11-08 14:09:41 -0600

    repo creado

---
+++ JSFProyect/src/main/java/net/symbiotic/co/jsfex/UserBean.java
@@ -0,0 +1,20 @@
+package net.symbiotic.co.jsfex;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean
+@SessionScoped
+public class UserBean {
+ 
+    private String name;
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+}
