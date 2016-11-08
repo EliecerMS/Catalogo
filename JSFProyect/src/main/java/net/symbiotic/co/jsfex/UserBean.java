51d7a30be14c9f70af603069163cbb416b8077ee
symbiotesting <alisoncoronado53@gmail.com>
2016-11-08 15:44:28 -0600

    cambio

--- JSFProyect/src/main/java/net/symbiotic/co/jsfex/UserBean.java
+++ JSFProyect/src/main/java/net/symbiotic/co/jsfex/UserBean.java
@@ -1,14 +1,20 @@
 package net.symbiotic.co.jsfex;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
  
+
 @ManagedBean
 @SessionScoped
 public class UserBean {
  
     private String name;
  
+    @PostConstruct
+    public void init(){
+    	
+    }
     public String getName() {
         return name;
     }
