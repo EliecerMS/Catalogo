94fdc90dd0c935280620cd050fef4037b50711da
Eliecer <Eliecer>
2016-12-02 09:43:46 -0600

    Todo

--- JSFProyect/src/main/webapp/resources/js/menu.js
+++ JSFProyect/src/main/webapp/resources/js/menu.js
@@ -1,12 +1,14 @@
 $(document).ready(function($) {
+	var myVar;
   $('#mostrar-nav').on('click', function () {
       $('#menu').toggleClass('mostrar');
       $('#loading').show();
-      $('#loading').delay(3000).hide(500);
-      $('#results').show();
+      $('#loading').delay(1000).hide(500);
     })
 });
 
+	
+
     function myFunction(x) {
-    x.classList.toggle("change");
-  }
+    	x.classList.toggle("change");
+    }
